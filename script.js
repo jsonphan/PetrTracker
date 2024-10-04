1ed0c1f2a69ca16520400837ec84e21a1ba2de91
Jason Phan <jsonphan@gmail.com>
2024-10-03 19:31:40 -0700

    Imported Firebase and necessary functions.

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6oNqInbKnwrcTykbi-ZmNpEs17VEqvzU",
+  authDomain: "start-4bda5.firebaseapp.com",
+  projectId: "start-4bda5",
+  storageBucket: "start-4bda5.appspot.com",
+  messagingSenderId: "274359714119",
+  appId: "1:274359714119:web:f7696f8c4fabd3c50bcc62",
+  measurementId: "G-VQT2N42C6S",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function loginFunction(){
   
